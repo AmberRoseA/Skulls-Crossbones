b097893c9e7ca7214272bbad758c927a25d5d86d
AmberRoseA <>
2023-12-20 15:19:25 +0000

    Added increments for scoreboard in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 const box = document.querySelectorAll(".box");
 const statusText = document.querySelector('#status-text');
-const scorePoints = document.querySelector('.score-options');
 const restartBtn = document.querySelector('#restartBtn');
-
 const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -13,75 +11,36 @@ const winConditions = [
     [0, 4, 8],
     [2, 4, 6]
 ];
+
 let options = ["", "", "", "", "", "", "", "", ""];
-let x = document.querySelector('.playerX');
-let o = document.querySelector('.playerO')
-let currentPlayer = x;
+let currentPlayer = ;
 let running = false;
-
-initializeGame();
-
-function initializeGame() {
-    box.forEach(box => box.addEventListener("click", boxClicked));
-    restartBtn.addEventListener("click", restartGame);
-    statusText.textContent = `${currentPlayer}'s turn`;
-    running = true;
-}
-
-function boxClicked() {
-    const boxIndex = this.getAttribute("boxIndex");
-
-    if (options[boxIndex] != "" || !running) {
-        return;
-    }
-
-    updateBox(this, boxIndex);
-    checkWinner();
-}
-
-function updateBox(box, index) {
-    options[index] = currentPlayer;
-    box.textContent = currentPlayer;
-}
-
-function changePlayer() {
-    currentPlayer = (currentPlayer == X) ? O : X;
-    statusText.textContent = `${currentPlayer}'s turn`;
+/**
+ * Gets the current tally of Games one by Skull from the DOM and increments it by 1
+ */
+function incrementSkullScore() {
+    let oldScore = parseInt(document.getElementById("skull-score").innerText);
+    document.getElementById("skull-score").innerText = ++oldScore;
 
 }
 
-function checkWinner() {
-    let roundWon = false;
+/**
+ * Gets the current tally of Games one by Crossbones from the DOM and increments it by 1
+ */
+function incrementBoneScore() {
 
-    for (let i = 0; i < winConditions.length; i++) {
-        const condition = winConditions[i];
-        const boxA = options[condition[0]];
-        const boxB = options[condition[1]];
-        const boxC = options[condition[2]];
-
-        if (boxA == "" || boxB == "" || boxC == "") {
-            continue;
-        }
-        if (boxA == boxB && boxB == boxC) {
-            roundWon = true;
-            break;
-        }
-    }
-
-    if (roundWon) {
-        statusText.textContent = `${currentPlayer} wins!`;
-        running = false;
-    }
-    else if (!options.includes("")) {
-        statusText.textContent = `Tie!`;
-        running = false;
-    }
-    else {
-        changePlayer();
-}
-
-function updateScorePoints(){
+    let oldScore = parseInt(document.getElementById("bone-score").innerText);
+    document.getElementById("bone-score").innerText = ++oldScore;
 
 }
 
-function restartGame
+/**
+ * Gets the current tally of Tied Game from the DOM and increments it by 1
+ */
+function incrementTieScore() {
+
+    let oldScore = parseInt(document.getElementById("tie-score").innerText);
+    document.getElementById("tie-score").innerText = ++oldScore;
+
+}
+
