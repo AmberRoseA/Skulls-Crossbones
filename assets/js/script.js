ddf16c0d9bea8534a18687e2a19574994a0f63eb
AmberRoseA <>
2023-12-20 02:18:23 +0000

    added code for checking the winner function

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,15 +38,46 @@ function boxClicked(){
 
 }
 
-function updateBox(box, index){
+function updateBox(box, data){
+    options[data] = currentPlayer;
+    box.textContent = currentPlayer;
 
 }
 
 function changePlayer(){
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`;
 
 }
 
 function checkWinner(){
+    let roundWon = false;
+
+    for(let i = 0; i < winConditions.length; i++){
+        const condition = winConditions[i];
+        const boxA = options[condition[0]];
+        const boxB = options[condition[1]];
+        const boxc = options[condition[2]];
+
+        if(boxA == "" || boxB == "" || boxC == ""){
+            continue;
+        }
+
+        if(boxA == boxB && boxb == boxC){
+            roundWon = true;
+            break;
+        }
+    }
+
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} Wins!`;
+        running = false;
+    } else if(!options.includes("")){
+        statusText.textContent = `Tie!`;
+        running = false;
+    } else{
+        changePlayer();
+    }
 
 }
 
